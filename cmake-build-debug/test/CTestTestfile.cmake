31719b0ce524f0c48eb0ac5851271bf7a9369f74
Ivzond <141146338+Ivzond@users.noreply.github.com>
2024-07-30 21:24:32 +0300

    Delete cmake-build-debug directory

--- cmake-build-debug/test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user/CLionProjects/cpp-async/test
-# Build directory: /home/user/CLionProjects/cpp-async/cmake-build-debug/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
