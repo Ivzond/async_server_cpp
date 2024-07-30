35b35c06d6549ec87e01a22935f452277ba3ec2e
Ivzond <G9207807332@yandex.ru>
2024-07-30 21:22:33 +0300

    Initial

---
+++ cmake-build-debug/test/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/user/CLionProjects/cpp-async/test
+# Build directory: /home/user/CLionProjects/cpp-async/cmake-build-debug/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
